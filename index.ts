514ddc598a29e40c0e04251bca4cb06fe471c9d8
Mike M <mikester70387@gmail.com>
2023-03-06 16:51:45 -0500

    Added more customizability to the flags provided to fping

--- index.ts
+++ index.ts
@@ -11,16 +11,27 @@ type PingHostAlive = PingHostBase & {
 
 export type PingHostResponse = PingHostBase | PingHostAlive;
 
+export type PingOptions = {
+    [key: string]: string;
+}
+
 /**
  * Attempts to ping provided hostnames,
  * and returns an array of PingHostResponse objects
  * representing the status of each host.
  * 
  * @param hosts the list of hostnames to ping
+ * @param timeout the timeout in milliseconds
+ * @param backoff the backoff multiplier
+ * @param retry the number of retries
+ * @param flags any other flags as defined in the manpage
+ * 
+ * @see fping documentation: https://fping.org/fping.1.html
+ * 
  * @returns the responses in the form of PingHostResponse objects
  */
-export default async (...hosts: string[]): Promise<PingHostResponse[]> => 
-    new Promise((res, _rej) => exec(`fping -e -t 1500 ${hosts.join(' ')}`, (err, stdout, stderr) => {
+export default async (hosts: string[], timeout = 1500, backoff = 1.5, retry = 3, flags?: PingOptions): Promise<PingHostResponse[]> => 
+    new Promise((res, _rej) => exec(`fping -e -t ${timeout} -B ${backoff} -r ${retry} ${Object.entries(flags ?? {}).map(([key, val]) => `${key} ${val}`).join(' ')} ${hosts.join(' ')}`, (err, stdout, stderr) => {
         let lines = stdout
             .concat(stderr)
             .toString()
